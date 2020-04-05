73beb582ac6adceaddee9505dee5d6be4c3f1e32
Robson Venturin <robventurin@gmail.com>
2020-04-05 10:08:06 -0300

    Alterado página inicial

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return redirect()->route('listar_clientes');
+    return redirect()->route('listar_vendas');
 });
 
 /* Rotas para clientes */
